e1126b593896811fb2bf1cd64773b006ff446f10
anuprshetty <anuprshetty@gmail.com>
2022-02-09 22:02:09 +0530

    moved grpc_server_handler to top in app.py

--- rest_grpc_dual_mode/app.py
+++ rest_grpc_dual_mode/app.py
@@ -5,10 +5,11 @@ from grpc_services.grpc_utilities import grpc_server
 app = Flask(__name__)
 rest_port = 5000
 
+grpc_server_handler = grpc_server.serve()
+# grpc_server_handler.wait_for_termination()
+
 from rest_services import rest_get_greetings
 
 if __name__ == "__main__":
-    grpc_server = grpc_server.serve()
-    # server.wait_for_termination()
     print(f"REST Server is running at port {rest_port}...")
     app.run(host='0.0.0.0', port=rest_port, debug=True)
--- rest_grpc_dual_mode/grpc_services/grpc_utilities/grpc_server.py
+++ rest_grpc_dual_mode/grpc_services/grpc_utilities/grpc_server.py
@@ -1,13 +1,26 @@
 import grpc
+import os
 from concurrent import futures
 from .generated import rest_grpc_dual_mode_pb2_grpc as pb2_grpc
 from .generated import rest_grpc_dual_mode_pb2 as pb2
 from ..grpc_get_greetings import GreetingsGeneratorService
 
 
-def serve():
+grpc_max_workers = os.getenv("GRPC_MAX_WORKERS")
+if not grpc_max_workers:
+    grpc_max_workers = 1
+grpc_max_workers = int(grpc_max_workers)
+
+grpc_port = os.getenv("GRPC_PORT")
+if not grpc_port:
     grpc_port = 50051
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+print(f"grpc_max_workers: {grpc_max_workers}")
+print(f"grpc_port: {grpc_port}")
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=grpc_max_workers))
     pb2_grpc.add_GreetingsGeneratorServicer_to_server(GreetingsGeneratorService(), server)
     server.add_insecure_port(f'[::]:{grpc_port}')
     server.start()
