125acb71237d0d2e1603efee9e5f8b6c20e9317b
anuprshetty <anuprshetty@gmail.com>
2022-02-20 14:21:24 +0530

    reformatted app_runner

--- app_runner.sh
+++ app_runner.sh
@@ -6,10 +6,10 @@
 # [EVERY_TIME] source path/to/app_runner.sh path/to/app_runner.sh
 
 script_path="$PWD/$1" &&
-script_dir="$(dirname "$script_path")" &&
-cd "$script_dir" &&
-source venv/bin/activate &&
-# pip install -r requirements.txt &&
-# python -m grpc_tools.protoc --proto_path=./protobufs ./protobufs/rest_grpc_dual_mode.proto --python_out=./rest_grpc_dual_mode/grpc_services/grpc_utilities/generated --grpc_python_out=./rest_grpc_dual_mode/grpc_services/grpc_utilities/generated &&
-# python -m grpc_tools.protoc --proto_path=./protobufs ./protobufs/rest_grpc_dual_mode.proto --python_out=./rest_grpc_dual_mode/tests/load_tests/grpc/generated --grpc_python_out=./rest_grpc_dual_mode/tests/load_tests/grpc/generated # &&
-python ./rest_grpc_dual_mode/app.py
+    script_dir="$(dirname "$script_path")" &&
+    cd "$script_dir" &&
+    source venv/bin/activate &&
+    # pip install -r requirements.txt &&
+    # python -m grpc_tools.protoc --proto_path=./protobufs ./protobufs/rest_grpc_dual_mode.proto --python_out=./rest_grpc_dual_mode/grpc_services/grpc_utilities/generated --grpc_python_out=./rest_grpc_dual_mode/grpc_services/grpc_utilities/generated &&
+    # python -m grpc_tools.protoc --proto_path=./protobufs ./protobufs/rest_grpc_dual_mode.proto --python_out=./rest_grpc_dual_mode/tests/load_tests/grpc/generated --grpc_python_out=./rest_grpc_dual_mode/tests/load_tests/grpc/generated # &&
+    python ./rest_grpc_dual_mode/app.py
